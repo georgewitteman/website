c35f16359a6f5d760befa25dba667f1915d1b066
George Witteman <george@witteman.me>
2023-07-08 22:13:45 +0000

    Use a Router class again

--- routes/auth.js
+++ routes/auth.js
@@ -9,171 +9,156 @@ import {
   validateUserPassword,
 } from "../lib/user.js";
 import { config } from "../lib/config.js";
-import { createRoute } from "../lib/route.js";
 import { z } from "zod";
 import { getCurrentUser } from "../middleware/user.js";
+import { Router } from "../lib/Router.js";
 
-/**
- * @type {import("../lib/route.js").Route[]}
- */
-export const routes = [];
+export const router = new Router();
 
-routes.push(
-  createRoute("GET", "/auth/signup", async () => {
-    return new MyResponse().html(
-      render(
-        await documentLayout({
-          title: "Sign up",
-          main: [
-            h("h1", {}, ["Sign Up"]),
-            h("form", { method: "POST", action: "/auth/signup" }, [
-              h("label", {}, [
-                "Email",
-                h("input", { name: "email", type: "email" }),
-              ]),
-              h("label", {}, [
-                "Password",
-                h("input", { name: "password", type: "password" }),
-              ]),
-              h("button", { type: "submit" }, ["Sign Up"]),
+router.get("/auth/signup", async () => {
+  return new MyResponse().html(
+    render(
+      await documentLayout({
+        title: "Sign up",
+        main: [
+          h("h1", {}, ["Sign Up"]),
+          h("form", { method: "POST", action: "/auth/signup" }, [
+            h("label", {}, [
+              "Email",
+              h("input", { name: "email", type: "email" }),
             ]),
-          ],
-        }),
-      ),
-    );
-  }),
-);
-
-routes.push(
-  createRoute("POST", "/auth/signup", async (req) => {
-    const { email, password } = z
-      .object({
-        email: z.string().email(),
-        password: z.string(),
-      })
-      .parse(await req.body());
-    const result = await createUser({ email, password });
-    if (!result.created) {
-      return new MyResponse().status(400).body(result.reason);
-    }
-
-    return MyResponse.redirectFound("/auth/signin");
-  }),
-);
-
-routes.push(
-  createRoute("GET", "/auth/signin", async () => {
-    return new MyResponse().html(
-      render(
-        await documentLayout({
-          title: "Sign in",
-          main: [
-            h("h1", {}, ["Sign in"]),
-            h("form", { method: "POST", action: "/auth/signin" }, [
-              h("label", {}, [
-                "Email",
-                h("input", { name: "email", type: "email" }),
-              ]),
-              h("label", {}, [
-                "Password",
-                h("input", { name: "password", type: "password" }),
-              ]),
-              h("button", { type: "submit" }, ["Sign in"]),
+            h("label", {}, [
+              "Password",
+              h("input", { name: "password", type: "password" }),
             ]),
-          ],
-        }),
-      ),
-    );
-  }),
-);
-
-routes.push(
-  createRoute("POST", "/auth/signin", async (req) => {
-    const { email, password } = z
-      .object({
-        email: z.string().email(),
-        password: z.string(),
-      })
-      .parse(await req.body());
-    const user = await getUserByEmail(email);
-
-    if (!user) {
-      return new MyResponse()
-        .status(404)
-        .body("Unable to find a user for this email");
-    }
-
-    const isValid = await validateUserPassword(user, password);
-    if (!isValid) {
-      return new MyResponse().status(401).body("Invalid password");
-    }
-
-    const session = await createSession(user.id);
-
-    return MyResponse.redirectFound(`/auth/profile`).header(
-      "Set-Cookie",
-      cookie.serialize("id", session.id, {
-        expires: session.expiresAt,
-        httpOnly: true,
-        path: "/",
-        sameSite: "strict",
-        secure: config.session.secure,
+            h("button", { type: "submit" }, ["Sign Up"]),
+          ]),
+        ],
       }),
-    );
-  }),
-);
+    ),
+  );
+});
 
-routes.push(
-  createRoute("GET", "/auth/profile", async () => {
-    const user = getCurrentUser();
-    if (!user) {
-      return new MyResponse().status(401).body("Not signed in");
-    }
+router.post("/auth/signup", async (req) => {
+  const { email, password } = z
+    .object({
+      email: z.string().email(),
+      password: z.string(),
+    })
+    .parse(await req.body());
+  const result = await createUser({ email, password });
+  if (!result.created) {
+    return new MyResponse().status(400).body(result.reason);
+  }
 
-    return new MyResponse().html(
-      render(
-        await documentLayout({
-          title: `Profile: ${user.email}`,
-          main: [
-            h("h1", {}, [user.email]),
-            h("dl", {}, [
-              h("dt", {}, ["id"]),
-              h("dd", {}, [user.id.toString()]),
-              h("dt", {}, ["email"]),
-              h("dd", {}, [user.email]),
-              h("dt", {}, ["password_salt"]),
-              h("dd", {}, [user.password.salt.toString("hex")]),
-              h("dt", {}, ["password_hash"]),
-              h("dd", {}, [user.password.hash.toString("hex")]),
+  return MyResponse.redirectFound("/auth/signin");
+});
+
+router.get("/auth/signin", async () => {
+  return new MyResponse().html(
+    render(
+      await documentLayout({
+        title: "Sign in",
+        main: [
+          h("h1", {}, ["Sign in"]),
+          h("form", { method: "POST", action: "/auth/signin" }, [
+            h("label", {}, [
+              "Email",
+              h("input", { name: "email", type: "email" }),
             ]),
-          ],
-        }),
-      ),
-    );
-  }),
-);
-
-routes.push(
-  createRoute("GET", "/auth/logout", async (req) => {
-    const redirectUrl = "/auth/signin";
-    const sessionId = req.cookies.id;
-    if (!sessionId) {
-      return MyResponse.redirectFound(redirectUrl);
-    }
-    await expireSession(sessionId);
-
-    const newExpiration = new Date();
-    newExpiration.setHours(newExpiration.getHours() - 1);
-
-    return MyResponse.redirectFound(redirectUrl).header(
-      "Set-Cookie",
-      cookie.serialize("id", "", {
-        expires: newExpiration,
-        httpOnly: true,
-        path: "/",
-        sameSite: "strict",
-        secure: config.session.secure,
+            h("label", {}, [
+              "Password",
+              h("input", { name: "password", type: "password" }),
+            ]),
+            h("button", { type: "submit" }, ["Sign in"]),
+          ]),
+        ],
       }),
-    );
-  }),
-);
+    ),
+  );
+});
+
+router.post("/auth/signin", async (req) => {
+  const { email, password } = z
+    .object({
+      email: z.string().email(),
+      password: z.string(),
+    })
+    .parse(await req.body());
+  const user = await getUserByEmail(email);
+
+  if (!user) {
+    return new MyResponse()
+      .status(404)
+      .body("Unable to find a user for this email");
+  }
+
+  const isValid = await validateUserPassword(user, password);
+  if (!isValid) {
+    return new MyResponse().status(401).body("Invalid password");
+  }
+
+  const session = await createSession(user.id);
+
+  return MyResponse.redirectFound(`/auth/profile`).header(
+    "Set-Cookie",
+    cookie.serialize("id", session.id, {
+      expires: session.expiresAt,
+      httpOnly: true,
+      path: "/",
+      sameSite: "strict",
+      secure: config.session.secure,
+    }),
+  );
+});
+
+router.get("/auth/profile", async () => {
+  const user = getCurrentUser();
+  if (!user) {
+    return new MyResponse().status(401).body("Not signed in");
+  }
+
+  return new MyResponse().html(
+    render(
+      await documentLayout({
+        title: `Profile: ${user.email}`,
+        main: [
+          h("h1", {}, [user.email]),
+          h("dl", {}, [
+            h("dt", {}, ["id"]),
+            h("dd", {}, [user.id.toString()]),
+            h("dt", {}, ["email"]),
+            h("dd", {}, [user.email]),
+            h("dt", {}, ["password_salt"]),
+            h("dd", {}, [user.password.salt.toString("hex")]),
+            h("dt", {}, ["password_hash"]),
+            h("dd", {}, [user.password.hash.toString("hex")]),
+          ]),
+        ],
+      }),
+    ),
+  );
+});
+
+router.get("/auth/logout", async (req) => {
+  const redirectUrl = "/auth/signin";
+  const sessionId = req.cookies.id;
+  if (!sessionId) {
+    return MyResponse.redirectFound(redirectUrl);
+  }
+  await expireSession(sessionId);
+
+  const newExpiration = new Date();
+  newExpiration.setHours(newExpiration.getHours() - 1);
+
+  return MyResponse.redirectFound(redirectUrl).header(
+    "Set-Cookie",
+    cookie.serialize("id", "", {
+      expires: newExpiration,
+      httpOnly: true,
+      path: "/",
+      sameSite: "strict",
+      secure: config.session.secure,
+    }),
+  );
+});
--- routes/migrations.js
+++ routes/migrations.js
@@ -8,13 +8,10 @@ import {
   listMigrations,
   runMigration,
 } from "../lib/migrations.js";
-import { createRoute, route } from "../lib/route.js";
-import { getCurrentUser } from "../middleware/user.js";
+import { route } from "../lib/route.js";
+import { Router } from "../lib/Router.js";
 
-/**
- * @type {import("../lib/route.js").Route[]}
- */
-export const routes = [];
+export const router = new Router();
 
 /**
  * @param {string | undefined} name
@@ -30,119 +27,102 @@ async function migrationNotFound(name) {
   );
 }
 
-routes.push(
-  createRoute("GET", "/migrations", async () => {
-    const migrations = await listMigrations();
-    return new MyResponse().html(
-      render(
-        await documentLayout({
-          title: "Migrations",
-          main: [
-            h("h1", {}, ["Migrations"]),
-            h("table", { class: "table" }, [
-              h("thread", {}, [
+router.get("/migrations", async () => {
+  const migrations = await listMigrations();
+  return new MyResponse().html(
+    render(
+      await documentLayout({
+        title: "Migrations",
+        main: [
+          h("h1", {}, ["Migrations"]),
+          h("table", { class: "table" }, [
+            h("thread", {}, [
+              h("tr", {}, [
+                h("th", { scope: "col" }, ["Name"]),
+                h("th", { scope: "col" }, ["Completed On"]),
+              ]),
+            ]),
+            h(
+              "tbody",
+              {},
+              migrations.map(({ name, completedOn }) =>
                 h("tr", {}, [
-                  h("th", { scope: "col" }, ["Name"]),
-                  h("th", { scope: "col" }, ["Completed On"]),
+                  h("td", {}, [h("a", { href: `/migration/${name}` }, [name])]),
+                  h("td", {}, [completedOn?.toLocaleString() ?? ""]),
                 ]),
-              ]),
-              h(
-                "tbody",
-                {},
-                migrations.map(({ name, completedOn }) =>
-                  h("tr", {}, [
-                    h("td", {}, [
-                      h("a", { href: `/migration/${name}` }, [name]),
-                    ]),
-                    h("td", {}, [completedOn?.toLocaleString() ?? ""]),
-                  ]),
-                ),
               ),
+            ),
+          ]),
+        ],
+      }),
+    ),
+  );
+});
+
+router.get("/migration/:name", async (req) => {
+  const name = z.object({ name: z.string().optional() }).parse(req.params).name;
+  if (!name) {
+    logger.warn(`Migration name is empty or nullish: ${name ?? typeof name}`);
+    return migrationNotFound(name);
+  }
+
+  const migration = await getMigration(name);
+  if (!migration) {
+    logger.warn(`Migration not found: ${name}`);
+    return migrationNotFound(name);
+  }
+
+  return new MyResponse().html(
+    render(
+      await documentLayout({
+        title: "Migrations",
+        main: [
+          h("h1", {}, [`Migration: ${migration.name}`]),
+          h("a", { href: "/migrations" }, ["\u2039 List migrations"]),
+          h("dl", {}, [
+            h("dt", {}, ["Name"]),
+            h("dd", {}, [migration.name]),
+
+            h("dt", {}, ["Completed On"]),
+            h("dd", {}, [
+              migration.completedOn?.toLocaleString() ?? "Not completed",
             ]),
-          ],
-        }),
-      ),
-    );
-  }),
-);
 
-routes.push(
-  createRoute("GET", "/migration/:name", async (req) => {
-    const name = z
-      .object({ name: z.string().optional() })
-      .parse(req.params).name;
-    if (!name) {
-      logger.warn(`Migration name is empty or nullish: ${name ?? typeof name}`);
-      return migrationNotFound(name);
-    }
+            h("dt", {}, ["Content"]),
+            h("dd", {}, [h("pre", {}, [h("code", {}, [migration.content])])]),
+          ]),
+          h("form", { method: "POST" }, [
+            h(
+              "button",
+              { type: "submit", disabled: Boolean(migration.completedOn) },
+              ["Run migration"],
+            ),
+          ]),
+        ],
+      }),
+    ),
+  );
+});
 
-    const migration = await getMigration(name);
-    if (!migration) {
-      logger.warn(`Migration not found: ${name}`);
-      return migrationNotFound(name);
-    }
+router.post("/migration/:name", async (req) => {
+  const name = z.object({ name: z.string().optional() }).parse(req.params).name;
+  if (!name) {
+    logger.warn(`Migration empty or not a string: ${name ?? typeof name}`);
+    return MyResponse.redirectFound(route("migrations"));
+  }
 
-    return new MyResponse().html(
-      render(
-        await documentLayout({
-          title: "Migrations",
-          main: [
-            h("h1", {}, [`Migration: ${migration.name}`]),
-            h("a", { href: "/migrations" }, ["\u2039 List migrations"]),
-            h("dl", {}, [
-              h("dt", {}, ["Name"]),
-              h("dd", {}, [migration.name]),
-
-              h("dt", {}, ["Completed On"]),
-              h("dd", {}, [
-                migration.completedOn?.toLocaleString() ?? "Not completed",
-              ]),
-
-              h("dt", {}, ["Content"]),
-              h("dd", {}, [h("pre", {}, [h("code", {}, [migration.content])])]),
-            ]),
-            h("form", { method: "POST" }, [
-              h(
-                "button",
-                { type: "submit", disabled: Boolean(migration.completedOn) },
-                ["Run migration"],
-              ),
-            ]),
-          ],
-        }),
-      ),
-    );
-  }),
-);
-
-routes.push(
-  createRoute("POST", "/migration/:name", async (req) => {
-    const user = getCurrentUser();
-    if (!user) {
-      return new MyResponse().status(401).body("Not signed in");
-    }
-
-    const name = z
-      .object({ name: z.string().optional() })
-      .parse(req.params).name;
-    if (!name) {
-      logger.warn(`Migration empty or not a string: ${name ?? typeof name}`);
-      return MyResponse.redirectFound(route("migrations"));
-    }
-
-    const migration = await getMigration(name);
-    if (!migration) {
-      logger.warn(`Migration not found (is string): ${name}`);
-      return MyResponse.redirectFound(route("migration", name));
-    }
-
-    if (migration.completedOn) {
-      return new MyResponse(404).html(`Migration already completed on
-    ${migration.completedOn.toLocaleString()}`);
-    }
-
-    await runMigration(name);
-
+  const migration = await getMigration(name);
+  if (!migration) {
+    logger.warn(`Migration not found (is string): ${name}`);
     return MyResponse.redirectFound(route("migration", name));
-  }),
-);
+  }
+
+  if (migration.completedOn) {
+    return new MyResponse(404).html(`Migration already completed on
+    ${migration.completedOn.toLocaleString()}`);
+  }
+
+  await runMigration(name);
+
+  return MyResponse.redirectFound(route("migration", name));
+});
