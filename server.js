03411d8a3e42f521ae4f63c0006eab4be567b78d
George Witteman <george@witteman.me>
2023-11-20 01:45:31 +0000

    Skip database cleanup in production

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { logger } from "./lib/logger.js";
 import { app } from "./lib/app.js";
 import { createServer } from "http";
 import { createTerminus } from "@godaddy/terminus";
+import { config } from "./lib/config.js";
 
 const PORT = 8080;
 
@@ -12,14 +13,19 @@ const server = createServer(app);
 
 /** @type {import("@godaddy/terminus").TerminusOptions} */
 const options = {
+  signals: ["SIGINT", "SIGTERM"],
   onSignal: async () => {
     logger.info("Server is starting cleanup");
 
-    try {
-      await getPool().end();
-      logger.info("Successfully ended the database connection pool");
-    } catch (e) {
-      logger.error("Failed to end the database connection pool", e);
+    if (config.database.exists) {
+      try {
+        await getPool().end();
+        logger.info("Successfully ended the database connection pool");
+      } catch (e) {
+        logger.error("Failed to end the database connection pool", e);
+      }
+    } else {
+      logger.info("No database configured. Skipping database cleanup");
     }
   },
   onShutdown: async () => {
@@ -33,44 +39,3 @@ createTerminus(server, options);
 server.listen(PORT, "0.0.0.0", () => {
   logger.info("listening on", server.address());
 });
-
-// const httpTerminator = createHttpTerminator(server);
-
-// let forceClose = false;
-
-// /**
-//  * @param {string} signal
-//  */
-// async function shutdownInner(signal) {
-//   if (forceClose) {
-//     logger.info("Forcing exit");
-//     process.exit(1);
-//   }
-//   forceClose = true;
-//   logger.info(`signal ${signal}`);
-
-//   // https://nodejs.org/docs/latest-v18.x/api/net.html#serverclosecallback
-//   try {
-//     await httpTerminator.terminate();
-//     logger.info("Successfully terminated the server");
-//   } catch (e) {
-//     logger.error("Failed to terminate the server", e);
-//   }
-
-//   try {
-//     await getPool().end();
-//     logger.info("Successfully ended the database connection pool");
-//   } catch (e) {
-//     logger.error("Failed to end the database connection pool", e);
-//   }
-// }
-
-// /**
-//  * @param {string} signal
-//  */
-// function shutdownOuter(signal) {
-//   shutdownInner(signal);
-// }
-
-// process.on("SIGINT", shutdownOuter);
-// process.on("SIGTERM", shutdownOuter);
