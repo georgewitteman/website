ef4e5f982b00b1c23c990d9c6f965b74a5c09530
George Witteman <george@witteman.me>
2023-11-21 15:27:32 +0000

    Add tailwind

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -6,6 +6,7 @@
 const config = {
   trailingComma: "all",
   htmlWhitespaceSensitivity: "strict",
+  plugins: ["prettier-plugin-tailwindcss"],
 };
 
 module.exports = config;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./views/**/*.njk"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
